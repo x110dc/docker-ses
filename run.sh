4c5cbfd823202daf353f7623969df1a27345ace6
Daniel Craigmile <dcraigmile@texastribune.org>
2014-06-27 14:30:04 -0500

    change sed delimiter

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sed -i "s/SES_USERNAME/${SES_USERNAME}/g" /etc/postfix/sasl_passwd
-sed -i "s/SES_API_KEY/${SES_API_KEY}/g" /etc/postfix/sasl_passwd
+sed -i "s,SES_USERNAME,${SES_USERNAME},g" /etc/postfix/sasl_passwd
+sed -i "s,SES_API_KEY,${SES_API_KEY},g" /etc/postfix/sasl_passwd
 
 /usr/sbin/postmap /etc/postfix/sasl_passwd
 
